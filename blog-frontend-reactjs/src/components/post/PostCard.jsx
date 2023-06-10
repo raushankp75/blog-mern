1211cc38bb1953cb4113087c341d2bbc4f630037
Raushan <raushankp75@gmail.com>
2023-06-10 23:35:17 +0530

    Dynamically from database show Blogs for User role using map function at PostCard component

--- blog-frontend-reactjs/src/components/post/PostCard.jsx
+++ blog-frontend-reactjs/src/components/post/PostCard.jsx
@@ -1,53 +1,106 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FcLikePlaceholder, FcLike } from 'react-icons/fc'
 import { FaComment } from 'react-icons/fa'
+import axios from 'axios';
+
+
+// format date
+import format from 'date-fns/format'
+
+
+
 
 const PostCard = () => {
+
+    const [data, setData] = useState([]);
+
+    // view post list
+    const getPost = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/posts/view', {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                withCredentials: true,    // IMPORTANT!!!
+            })
+            setData(response.data.posts);
+            console.log(response.data.posts)
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    useEffect(() => {
+        getPost();
+    }, [])
+
+
+
+
     return (
         <>
-            <div className='p-4 flex flex-col gap-3 shadow-perfect rounded-2xl'>
-                <div className='flex flex-row gap-4'>
-                    <img src="https://www.nicepng.com/png/detail/933-9332131_profile-picture-default-png.png" alt="image" className='rounded-full w-14 h-14' />
-                    <div className='flex flex-col justify-center'>
-                        <h1 className='text-lg font-semibold'>Testing Blog</h1>
-                        <small><span className='font-semibold text-blue-500'>Created by : </span><span>Raushan Kumar</span></small>
-                    </div>
-                </div>
+            {
+                data && data.map((post, index) => {
+                    return (
 
-                <hr />
+                        <div className='p-4 flex flex-col gap-3 shadow-perfect rounded-2xl' key={index}>
 
-                <div>
-                    <small><span className='font-semibold text-blue-500'>Category : </span><span>Tech</span></small>
-                </div>
+                            <div className='flex flex-row gap-4'>
+                                {/* <img src={post?.image?.url} alt="image" className='rounded-full w-14 h-14' /> */}
+                                <div className='flex flex-col justify-center'>
+                                    <h1 className='text-lg font-semibold'>{post?.title}</h1>
+                                    <small><span className='font-semibold text-blue-500'>Created by : </span><span>{post?.postedBy?.name}</span></small>
+                                </div>
+                            </div>
 
-                <div>
-                    <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg" alt="" className='w-fit object-cover rounded-sm' />
-                </div>
+                            <hr />
 
-                <div>
-                    <p className='text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam neque possimus non culpa voluptate inventore excepturi, obcaecati natus </p>
-                </div>
 
-                <hr />
 
-                <div className='flex justify-between'>
-                    <p className='text-xs'>View Detail</p>
-                    <p className='text-xs'>june 6 2023</p>
-                </div>
+                            <div>
+                                <img src={post?.image?.url} alt="" className='w-fit object-cover rounded-sm' />
+                            </div>
 
-                <div className='flex flex-row justify-between'>
-                    <div className='flex flex-row gap-2 items-center'>
-                        <FcLikePlaceholder size={30} className='cursor-pointer' /> <span className='font-semibold'>2 Likes</span>
-                    </div>
-                    {/* <FcLike size={30} className='cursor-pointer' /> */}
+                            <div>
+                                <p className='text-sm' dangerouslySetInnerHTML={{ __html: post?.content }}></p>
+                            </div>
 
-                    <div className='flex flex-row gap-2 items-center'>
-                        <FaComment size={30} className='cursor-pointer' /> <span className='font-semibold'>5 Comments</span>
-                    </div>
-                </div>
-            </div>
+                            <hr />
+
+                            <div className='flex justify-between'>
+                                <p className='text-xs'>View Detail</p>
+                                <p className='text-xs'>{format(new Date(post?.createdAt), 'MM/dd/yyyy, HH:MM')}</p>
+                            </div>
+
+                            <div className='flex flex-row justify-between'>
+                                <div className='flex flex-row gap-2 items-center'>
+                                    <FcLikePlaceholder size={30} className='cursor-pointer' /> <span className='font-semibold'>{post?.likes?.length} Likes</span>
+                                </div>
+
+
+                                <div className='flex flex-row gap-2 items-center'>
+                                    <FaComment size={30} className='cursor-pointer' /> <span className='font-semibold'>{post?.comments?.length} Comments</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    )
+                })
+            }
+
         </>
     )
 }
 
 export default PostCard
+
+
+
+
+
+
+{/* <div>
+                            <small><span className='font-semibold text-blue-500'>Category : </span><span>Tech</span></small>
+                        </div> */}
+
+{/* <FcLike size={30} className='cursor-pointer' /> */ }
--- blog-frontend-reactjs/src/pages/PostList.jsx
+++ blog-frontend-reactjs/src/pages/PostList.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PostCard from '../components/post/PostCard'
 import Banner from '../components/Banner'
 
+
 const PostList = () => {
+
+  
+
+  // const [loading, setLoading] = useState(false);
+
+
+  
+
+
+
   return (
     <>
       <div className=''>
         <Banner />
         <div className='grid md:grid-cols-3 grid-cols-1 gap-8'>
-         <PostCard />
-         <PostCard />
-         <PostCard />
-         <PostCard />
-         <PostCard />
-         <PostCard />
-         <PostCard />
+          {/* {
+            data && data.map((post, index) => {
+              <PostCard key={index} name={name} title={title} />
+            })
+          } */}
+
+          <PostCard />
+
+    {/* const { name, title, content, image, likes, comments, createdAt } = props */}
+
+
+
         </div>
       </div>
     </>
